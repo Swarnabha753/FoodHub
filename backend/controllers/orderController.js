0808c92ef825cb9af422347d0e98d92acf205c14
Swarnabha Nandi <nandiswarnabha@gmail.com>
2024-09-20 07:28:33 +0530

    building payment options

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -8,6 +8,9 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 //placing user order from frontend
 const placeOrder = async(req,res) => {
+
+    const frontend_url = "http://localhost:5173";
+
     try{
         const newOrder = new orderModel({
             userId: req.body.userId,
@@ -17,8 +20,38 @@ const placeOrder = async(req,res) => {
         })
         await newOrder.save();
         await userModel.findByIdAndUpdate(req.body.userId,{cartData:{}});
-    } catch(error) {
+        const line_items = req.body.items.map((items) => ({
+            price_data: {
+                currency: "$",
+                product_data:{
+                    name:item.name
+                },
+                unit_amount : item.price*100
+            },
+            quantity: item.quantity
+        }))
+        line_items.push({
+            price_data:{
+                currency:"$",
+                product_data:{
+                    name:"Delivery Charges"
+                },
+                unit_amount: 2*100
+            },
+            quantity: 1
+        })
+        const session = await stripe.checkout.sessions.create({
+            line_items: line_items,
+            mode: 'payment',
+            success_url: `${frontend_url}/verify?success=true&orderId=${newOrder._id}`,
+            cancel_url:`${frontend_url}/verify?success=false&orderId=${newOrder._id}`,
+        })
 
+        res.json({success:true, session_url: session.url});
+
+    } catch(error) {
+        console.log(error);
+        res.json({success: false, message: "Error"});
     }
 }
 
